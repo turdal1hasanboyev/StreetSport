e38a7b4c018015012c9bc5d703f65a33fbd398b9
Turdali <turdalihasanboyev70@gmail.com>
2025-04-05 11:49:33 +0500

    Writing custom permissions

---
+++ permissions/is_admin.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+from user.models import CustomUser
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.role == CustomUser.Roles.ADMIN)
+    
---
+++ permissions/is_owner.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+from user.models import CustomUser
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.role == CustomUser.Roles.OWNER)
